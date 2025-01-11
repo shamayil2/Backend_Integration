928c64f4a82ed909d61c5793f1109d52ea3dd0cb
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-01-12 03:57:46 +0530

    added delete functionality

--- BI1.2_CW/my-react-app/src/components/Movies.jsx
+++ BI1.2_CW/my-react-app/src/components/Movies.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react"
 import useFetch from "../useFetch"
 const Movies = () => {
  const {data,loading,error} = useFetch("http://localhost:3000/movies")
+ const [successMessage,setSuccessMessage] = useState("")
+
+ const clickHandler = async(movieId) => {
+
+    try{
+        const response = await fetch(`http://localhost:3000/movies/${movieId}`,{
+            method:"DELETE"
+        })
+
+        if(!response.ok){
+            throw "cant delete the movie"
+        }
+
+        const data = await response.json()
+        if(data){
+            setSuccessMessage("Movie Deleted Successfully")
+            window.location.reload()
+        }
+
+    }
+    catch(error){
+        console.log(error)
+    }
+
+ }
+
     return(
         <>
         <ul>
             {data?.map(movie=>(
-                <li key={movie._id}>{movie.title}</li>
+                <li key={movie._id}>{movie.title}{"  "}<button onClick={()=>clickHandler(movie._id)}>Delete</button></li>
             ))}
         </ul>
+        <p>{successMessage}</p>
         </>
     )
 
